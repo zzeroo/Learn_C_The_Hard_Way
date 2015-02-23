d4212d3e0b281a8ecee9d94570733f89766eb947
Stefan Müller <s.mueller@it.kls-glt.de>
2015-02-23 11:24:48 +0100

    Example libary from exercise 29

--- dbg.h
+++ dbg.h
@@ -2,13 +2,13 @@
 #define __dbg_h__
 
 #include <stdio.h>
-#include <error.h>
+#include <errno.h>
 #include <string.h>
 
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%D: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
@@ -21,7 +21,7 @@
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
-#define sentinel(M, ...) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
 #define check_mem(A) check((A), "Out of memory.")
 
